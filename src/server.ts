53b6eba8cf6ba446807424ea9c4f507d3ef5fcf9
Pedro Correia <correia.pedro@outlook.com>
2023-02-13 10:35:10 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const teste = await knex('sqlite_schema').select('*')
+  return teste
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
